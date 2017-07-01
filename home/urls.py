b1e579d146dcb1ded4520ffa1ebad3d0329f6c6b
user_namgit config --global user.email email_ide <14520315@gm.uit.edu.vn>
2017-07-01 07:14:53 +0700

    Buoi 3

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,9 @@ from . import views #Doan nay import cai view co ne!
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'congnghe', views.congnghe, name='congnghe'),
+    url(r'khoahoc', views.khoahoc, name='khoahoc'),
+    url(r'thethao', views.thethao, name='thethao'),
+    url(r'phapluat', views.phapluat, name='phapluat'),
+    url(r'game', views.game, name='game'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,6 +3,28 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
+
+#Home
 def index(request):
     ct={}
     return render(request,'home/base.html',ct)
+#Công nghệ
+def congnghe(request):
+    ct={}
+    return render(request,'home/congnghe/congnghe.html',ct)
+#Khoa học
+def khoahoc(request):
+    ct={}
+    return render(request,'home/khoahoc/khoahoc.html',ct)
+#Thể thao
+def thethao (request):
+    ct={}
+    return render(request,'home/thethao/thethao.html',ct)
+#Pháp luật
+def phapluat (request):
+    ct={}
+    return render(request,'home/phapluat/phapluat.html',ct)
+#Game
+def game (request):
+    ct={}
+    return render(request,'home/game/game.html',ct)
